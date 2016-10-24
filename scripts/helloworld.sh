96a81e26cd14a68f8e42a173d3bc56d52557e2ce
Radia Johnson <johnson.radia@gmail.com>
2016-10-24 15:12:08 -0400

    Create helloworld.sh

---
+++ scripts/helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\nHello World!"
+
+echo "Your current working directory is: $PWD ."
+
+echo -e "\nNow I will run the pwd command." 
+pwd
+
+echo -e "\nHave a great day!"
+echo "DONE."
+
