d7b1d8e42aa58f08a766de7f861600c926a430e3
Radia Johnson <johnson.radia@gmail.com>
2016-11-17 10:24:46 -0500

    Update star.sh

--- scripts/star.sh
+++ scripts/star.sh
@@ -8,7 +8,7 @@ module add SAMtools
 FILENAME=$1
 myDIR=$2
 mySCRIPTDIR=$3
-myGENOMEDIR="/RQexec/johnsonr/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes" # CHANGE TO YOU YOUR DIRECTORY
+myGENOMEDIR="/yourfullpathtodirectory/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes" 
 
 # Start the count variable from 0
 count=0
