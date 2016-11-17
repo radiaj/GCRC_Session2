d237a749e5a6234c55f279f684506a5d195284dd
Radia Johnson <johnson.radia@gmail.com>
2016-11-17 17:16:02 -0500

    Create getExampleFastq.sh

---
+++ getExampleFastq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $SCRATCH
+wget https://dl.dropboxusercontent.com/u/30969719/SRR1777876_accepted_hits_R1.fq.gz 
+wget https://dl.dropboxusercontent.com/u/30969719/SRR1777876_accepted_hits_R2.fq.gz 
+wget https://dl.dropboxusercontent.com/u/30969719/SRR1777877_accepted_hits_R1.fq.gz 
+wget https://dl.dropboxusercontent.com/u/30969719/SRR1777877_accepted_hits_R2.fq.gz 
+
+echo -e "\nDONE."
